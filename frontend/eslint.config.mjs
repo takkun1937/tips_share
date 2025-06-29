c1e192885f1a2910fa9e1ff18791682b6ed42e48
takkun1937 <takujin3811@gmail.com>
2025-06-29 10:01:21 +0900

    update: eslint security導入

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import pluginSecurity from 'eslint-plugin-security';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,6 +14,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   eslintPluginPrettierRecommended,
+  pluginSecurity.configs.recommended,
 ];
 
 export default eslintConfig;
